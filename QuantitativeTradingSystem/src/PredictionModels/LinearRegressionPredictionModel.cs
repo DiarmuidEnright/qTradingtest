611fba16f5e9ba92e5a14dbb596e0a83ffaf7a66
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-04 17:53:11 +0100

    fitting to line predict

--- QuantitativeTradingSystem/src/PredictionModels/LinearRegressionPredictionModel.cs
+++ QuantitativeTradingSystem/src/PredictionModels/LinearRegressionPredictionModel.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearRegression;
+
 namespace QuantitativeTradingSystem.PredictionModels
 {
     public class LinearRegressionPredictionModel : IPredictionModel
     {
         public double Predict(List<double> inputData)
         {
-            return 0.0;
+            var xData = new List<double>();
+            var yData = new List<double>();
+
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                xData.Add(i);
+                yData.Add(inputData[i]);
+            }
+
+            (double intercept, double slope) = SimpleRegression.Fit(xData.ToArray(), yData.ToArray());
+
+            return intercept + slope * inputData.Count;
         }
     }
 }
