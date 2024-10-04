168147f7cd43539f2f603b4a9f634fa87c1b3189
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-04 17:50:17 +0100

    Updated MAI using sys disp objects

--- QuantitativeTradingSystem/src/Indicators/MovingAverageIndicator.cs
+++ QuantitativeTradingSystem/src/Indicators/MovingAverageIndicator.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace QuantitativeTradingSystem.Indicators
 {
     public class MovingAverageIndicator : IIndicator
     {
+        private readonly int _period;
+
+        public MovingAverageIndicator(int period)
+        {
+            _period = period;
+        }
+
         public List<double> Calculate(List<double> values)
         {
-            return new List<double>();
+            var movingAverages = new List<double>();
+
+            for (int i = 0; i <= values.Count - _period; i++)
+            {
+                var average = values.Skip(i).Take(_period).Average();
+                movingAverages.Add(average);
+            }
+
+            return movingAverages;
         }
     }
 }
