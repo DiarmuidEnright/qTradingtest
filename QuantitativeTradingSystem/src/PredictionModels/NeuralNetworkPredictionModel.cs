c610b620b50d6980288cfc3d07938c56c64638da
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-04 17:53:58 +0100

    neural net basic train using accor temp obj

--- QuantitativeTradingSystem/src/PredictionModels/NeuralNetworkPredictionModel.cs
+++ QuantitativeTradingSystem/src/PredictionModels/NeuralNetworkPredictionModel.cs
@@ -1,10 +1,34 @@
+using System.Collections.Generic;
+using Accord.Neuro;
+using Accord.Neuro.Learning;
+using Accord.Neuro.Networks;
+
 namespace QuantitativeTradingSystem.PredictionModels
 {
     public class NeuralNetworkPredictionModel : IPredictionModel
     {
+        private readonly ActivationNetwork _network;
+        private readonly BackPropagationLearning _teacher;
+
+        public NeuralNetworkPredictionModel()
+        {
+            _network = new ActivationNetwork(new SigmoidFunction(), 1, 10, 1);
+            _teacher = new BackPropagationLearning(_network);
+        }
+
         public double Predict(List<double> inputData)
         {
-            return 0.0;
+            var inputs = new double[inputData.Count];
+            var outputs = new double[inputData.Count];
+
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                inputs[i] = inputData[i];
+                outputs[i] = inputData[i];
+            }
+
+            _teacher.RunEpoch(inputs, outputs);
+            return _network.Compute(new[] { inputData[^1] })[0];
         }
     }
 }
